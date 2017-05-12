ab9965623f2e7bf3b0bbd8a3d084b05f9b2c6d8c
Harrow <victor.anton@hotmail.fr>
2017-05-12 11:46:10 +0200

    first commit. saving portfolio for publishing on antonvictor.fr

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+console.log($("#liste"));
+$('#contact').on('click', function(){
+
+	$('#contact_p').css('display', 'inherit');
+	$('#accueil_p').css('display', 'none');
+
+})
+
+console.log($("#liste"));
+$('#accueil').on('click', function(){
+	$('#accueil_p').css('display', 'inherit');
+	$('#contact_p').css('display', 'none');
+
+})
+})
