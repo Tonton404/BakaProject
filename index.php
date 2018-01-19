308c75a1b74010cfec84e9a57de0a5559df3d0e6
Tonton404 <victor.anton@hotmail.fr>
2018-01-19 16:17:00 +0100

    commit for work in progress v2.0

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 	
+<?php
 	require_once("data.php");
  ?>
 
@@ -11,39 +11,34 @@
 		<script src="https://code.jquery.com/jquery-3.2.1.min.js";></script>
 		<script src="script.js"></script>
 		<span id="description-victor"> <?php echo $description ?> </span>
-	</head> 
+		<script src="/particles/particles.js"></script>
+		<script src="app.js"></script>
+	</head>
 
-	<body>
+	<body class="content">
 		<header>
-			<div id="photo">
-				<img src="<?php echo $profilimg ?>">
-			</div>	
 				<h2> <?php echo $h2header ?> </h2>
 		</header>
-	<main>
-	<div id="bloc"; padding: 0 0 50px 0;>
-		<div id="liste">
-			<ul>
-				<a href="#"><li id="accueil"> <?php echo $accueil ?> </li></a>
-				<a href="#"><li id="formation"> <?php echo $formation ?> </li></a>
-				<a href="#"><li id="contact"> <?php echo $contact ?> </li></a>
-				<!-- <li id="funfacts">Fun Facts</li> a mettre en place plus tard-->
-				<!-- <li id="livre">Livre d'or</li> a mettre en place plus tard-->
-			</ul>
-		</div>
-
-		<div id="texte">
-			<p id="accueil_p" > <?php echo $accueil_p ?> </p>
-
-			<p id="contact_p" > <?php echo $contact_p ?> </p>
-			
-			<p id="formation_p" > <?php echo $formation_p ?> </p>
-		</div>
-
-	</div>
-	</main>
 
 
+		<main>
+			<div id="bloc">
+				<div id="liste">
+					<ul>
+						<a href="#"><li id="accueil"> <?php echo $accueil ?> </li></a>
+						<a href="#"><li id="formation"> <?php echo $formation ?> </li></a>
+						<a href="#"><li id="contact"> <?php echo $contact ?> </li></a>
+					</ul>
+				</div>
+				<div id="texte">
+					<p id="accueil_p" > <?php echo $accueil_p ?> </p>
+					<p id="contact_p" > <?php echo $contact_p ?> </p>
+					<p id="formation_p" > <?php echo $formation_p ?> </p>
+				</div>
+			</div>
+		</main>
+
+		<div class="background" id='particles-js'></div>
 
 	<footer>
 			<p> <?php echo $footer ?> </p>
@@ -51,6 +46,5 @@
 
 	</body>
 
-</html>
 
-<!-- Mettre en place un Konami Code. une poulette ou Kenny McCormick traverse l'écran sinon Lorenzo pointe son bob -->
+</html>
